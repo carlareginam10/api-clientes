8a889dd1216fef9ea181e9b2e82596c0c6ea494f
CARLA REGINA MENDES <46799894+carlareginam10@users.noreply.github.com>
2022-03-31 22:09:26 -0300

    firt commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Disciplina APi Gateway & Integration
+## MBA em Full Stack Developer
+
+**API:** Clientes
+
+**Visão geral:** Essa aplicação tem por objetivo implementar uma API Rest (CRUD) de clientes
+
+
+**Tecnologias utilizadas:**
+- Spring Boot
+- Java 17
+- Maven
+- Postgres
+
+
---
+++ src/main/java/com/impacta/api/cliente/cliente/ClienteApplication.java
@@ -0,0 +1,13 @@
+package com.impacta.api.cliente.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteApplication.class, args);
+	}
+
+}
